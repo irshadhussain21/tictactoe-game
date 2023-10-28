1cde3380e3b4d3adc6c23da8ec96fdd20fd4f4d4
irshad <irshad@gmail.com>
2023-10-28 21:29:28 +0530

    highlighted winning combinations

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 
   const gamingBoard = history[currentMove];
 
-  const winner = calculateWinner(gamingBoard.squares);
+  const { winner, winningSquares } = calculateWinner(gamingBoard.squares);
 
   const handleSquareClick = clickedPosition => {
     if (gamingBoard.squares[clickedPosition] || winner) {
@@ -60,6 +60,7 @@ function App() {
       <Board
         squares={gamingBoard.squares}
         handleSquareClick={handleSquareClick}
+        winningSquares={winningSquares}
       />
 
       <button
